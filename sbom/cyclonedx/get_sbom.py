5c47a90a0250ebd14cd5defb845edee300b20aca
ilnarkhasanov <4sitescarp@gmail.com>
2025-04-02 20:36:18 +0300

    Add additional check for registry:2 in get_cyclonedx_sbom

--- sbom/cyclonedx/get_sbom.py
+++ sbom/cyclonedx/get_sbom.py
@@ -4,6 +4,10 @@ import uuid
 
 def get_cyclonedx_sbom(image_name: str) -> str:
     output_path = f"{uuid.uuid4()}.json"
+
+    if image_name == "registry:2":
+        image_name = "docker.io/library/" + image_name
+
     result: str = subprocess.run(
         ["syft", image_name, "-o", "cyclonedx-json"],
         capture_output=True,
