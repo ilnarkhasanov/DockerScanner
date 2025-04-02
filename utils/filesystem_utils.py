6e8afe3bd9a06c793de8f1bacdc784088ad7434c
ilnarkhasanov <4sitescarp@gmail.com>
2025-04-02 21:26:32 +0300

    Remove get_all_paths_in_folder

--- utils/filesystem_utils.py
+++ utils/filesystem_utils.py
@@ -1,19 +1,4 @@
 import json
-import os
-import typing
-
-
-def get_all_paths_in_folder(folder_path) -> list[str]:
-    all_paths: list[str] = []
-    for root, dirs, files in os.walk(folder_path):
-        for file in files:
-            full_path: str = typing.cast(
-                os.path.join(root, file),
-                str,
-            )
-            assert type(full_path) is str
-            all_paths.append(full_path)
-    return all_paths
 
 
 def read_json_file(json_output_path: str):
