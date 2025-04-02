1b9db393831f03673a472c27d136fc0ad7a2aed0
ilnarkhasanov <4sitescarp@gmail.com>
2025-04-02 20:47:13 +0300

    Refactor aggregate_snyk_results

--- static_analysis/snyk/aggregate.py
+++ static_analysis/snyk/aggregate.py
@@ -5,13 +5,13 @@ def aggregate_snyk_results(results: dict) -> list[CVE]:
     cve_list: list[CVE] = []
 
     if "vulnerabilities" not in results:
-        print()
+        return cve_list
 
     for vulnerability in results["vulnerabilities"]:
         cve_list.append(
             CVE(
                 code=vulnerability["identifiers"]["CVE"][0],
-                severity=vulnerability["nvdSeverity"],
+                severity=vulnerability["severity"],
                 product=vulnerability["name"],
                 version=vulnerability["version"],
             )
