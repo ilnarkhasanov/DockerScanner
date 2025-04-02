455f02a4fdd603b9093050f48f0cd3e226efafd4
ilnarkhasanov <4sitescarp@gmail.com>
2025-04-02 20:46:49 +0300

    Refactor run_sbom_without_sbom

--- static_analysis/snyk/run.py
+++ static_analysis/snyk/run.py
@@ -1,30 +1,17 @@
 import json
 import subprocess
-import uuid
 
-from sbom.cyclonedx.get_sbom import get_cyclonedx_sbom
+from sbom.cyclonedx.get_sbom import get_syft_cyclonedx_sbom
 
 
 def run_snyk_without_sbom(image_name: str, arm64: bool) -> dict:
-    if arm64:
-        result = subprocess.run(
-            ["snyk", "container", "test", "--json", image_name, "--platform=linux/arm64"],
-            capture_output=True,
-            text=True,
-        ).stdout.strip()
-    else:
-        result = subprocess.run(
-            ["snyk", "container", "test", "--json", image_name, "--platform=linux/amd64"],
-            capture_output=True,
-            text=True,
-        ).stdout.strip()
+    result = subprocess.run(
+        ["snyk", "container", "test", "--json", image_name, "--platform=linux/amd64"],
+        capture_output=True,
+        text=True,
+    ).stdout.strip()
 
     return json.loads(result)
-
-    # with open(output_path, "w") as txt:
-    #     txt.write(result)
-    #
-    # return output_path
 
 
 def run_snyk_with_sbom(image_name: str):
