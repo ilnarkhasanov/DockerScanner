e357ab2c74c33177571fd2feca3bfcf3b6343c09
ilnarkhasanov <4sitescarp@gmail.com>
2025-04-02 21:19:04 +0300

    Remove arm64 argument in entrypoint_run_snyk_without_sbom

--- static_analysis/snyk/entrypoint.py
+++ static_analysis/snyk/entrypoint.py
@@ -3,7 +3,7 @@ from static_analysis.snyk.aggregate import aggregate_snyk_results
 from static_analysis.snyk.run import run_snyk_without_sbom, run_snyk_with_sbom
 
 
-def entrypoint_run_snyk_without_sbom(image_name: str, arm64: bool) -> list[CVE]:
+def entrypoint_run_snyk_without_sbom(image_name: str) -> list[CVE]:
     print("Scanning by Snyk without SBOM...")
     scanning_result: dict = run_snyk_without_sbom(image_name)
     return aggregate_snyk_results(scanning_result)
