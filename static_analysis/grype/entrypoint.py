fb12a8557c0c29085bbc63e06d9077e2e97b3bd9
ilnarkhasanov <4sitescarp@gmail.com>
2025-04-06 22:14:27 +0300

    Add Grype entrypoints

--- static_analysis/grype/entrypoint.py
+++ static_analysis/grype/entrypoint.py
@@ -1,1 +1,13 @@
-# def run_trivy_without_sbom_entrypoint(image_name: str)
+from schemas.cve import CVE
+from static_analysis.grype.aggregate import aggregate_grype_results
+from static_analysis.grype.run import run_grype, run_grype_without_sbom
+
+
+def grype_entrypoint(image_name: str) -> list[CVE]:
+    grype_no_sbom_result = run_grype_without_sbom(image_name)
+    return aggregate_grype_results(grype_no_sbom_result)
+
+
+def grype_sbom_entrypoint(image_name: str) -> list[CVE]:
+    grype_result = run_grype(image_name)
+    return aggregate_grype_results(grype_result)
